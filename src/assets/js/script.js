521b70c2860d681adcbe5af12cfdde0018a2b16d
Sanj Prem <sanj.tools@gmail.com>
2018-07-08 14:15:04 -0400

    added function to hide select options and form validations

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('Awesome!');
+$(document).ready(function(){
+    $("select#selectEnquiry").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue){
+                $(".selectOption").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else{
+                $(".selectOption").hide();
+            }
+        });
+    }).change();
+});
+
+
+$("#formSubmit").click(function(event) {
+    var form = $("#formContact")
+    if (form[0].checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+    form.addClass('was-validated');
+        $.ajax({
+        type: "POST",
+        url: "sent-email.php"
+    });
+});
